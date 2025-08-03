1b71acfccac48361b81d232fec22233740c873f0
rvandanp <rvandana@cisco.com>
2025-08-03 11:55:58 -0700

    Changes made to explicitly pick src folders and files under that folder

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Triggered Lambda from GHA code')
+        'body': json.dumps('Triggered Lambda from latest code')
     }
